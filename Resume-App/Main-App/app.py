ac7e0746355b82b2ec4cc7d25f04a04bcb17425d
MUSTAFA892 <mustafatinwala6@gmail.com>
2025-04-25 12:06:34 +0530

    chnage the port in env

--- Resume-App/Main-App/app.py
+++ Resume-App/Main-App/app.py
@@ -568,4 +568,4 @@ def text_to_speech(text, output_path=None):
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 5000))  
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
